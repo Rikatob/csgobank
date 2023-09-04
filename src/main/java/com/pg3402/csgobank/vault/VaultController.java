e3af56063858a4a126d9cd20dbc11917518fd7f5
RikaTob <rikatob@hotmail.com>
2023-09-04 15:18:54 +0200

    VAULT CONTROLLER * Started creating a endpoint for showing a Vault with mock values.

--- src/main/java/com/pg3402/csgobank/vault/VaultController.java
+++ src/main/java/com/pg3402/csgobank/vault/VaultController.java
@@ -1,12 +1,34 @@
 package com.pg3402.csgobank.vault;
 
+import com.pg3402.csgobank.item.Item;
+import com.pg3402.csgobank.item.WearCategory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping("/vault")
 public class VaultController {
-    @GetMapping("/test")
+
+
+    @GetMapping("/")
+    Vault getVault(@RequestParam(name = "id", defaultValue = "1") String id) {
+        Item testItem = new Item(1,"AK","Fireserpent",233,1000,WearCategory.MINIMAL_WEAR);
+        Item testItem2 = new Item(2,"M4","Howl",111,2222,WearCategory.FACTORY_NEW);
+
+        List<Item> testList = new ArrayList<>();
+        testList.add(testItem);
+        testList.add(testItem2);
+
+        Vault testVault = new Vault(Long.parseLong(id),"Freddy",testList,testItem.getPrice() + testItem2.getPrice());
+
+        return testVault;
+    }
+
     public String test(@RequestParam(name = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
