443b76a92ab9a316e397a14afb596ea7f5687dd7
RikaTob <rikatob@hotmail.com>
2023-09-09 14:23:15 +0200

    TEST * Created simple test for verifying VaultController.

--- src/test/java/com/pg3402/csgobank/CsGoBankApplicationTests.java
+++ src/test/java/com/pg3402/csgobank/CsGoBankApplicationTests.java
@@ -1,13 +1,28 @@
 package com.pg3402.csgobank;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.pg3402.csgobank.vault.VaultController;
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CsGoBankApplicationTests {
 
+
+	@Autowired
+	private VaultController controller;
+
+	/**
+	 * Verify controller and services context.
+	 * @throws Exception
+	 */
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
+
 
 }
