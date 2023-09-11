88ebb4c30ee9b7c145ed23a41247f53533b3afe7
Rikatob <rikatob@hotmail.com>
2023-09-11 19:49:38 +0200

    * Created controller and a simple get endpoint

---
+++ transaction/src/main/java/com/pg3402/csgobank/transaction/transaction/TransactionController.java
@@ -0,0 +1,17 @@
+package com.pg3402.csgobank.transaction.transaction;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    @GetMapping
+    public @ResponseBody boolean validateTransaction() {
+        return true;
+    }
+}
