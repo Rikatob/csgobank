540bf1c54d474033debe3fc878750ea1c921f178
RikaTob <rikatob@hotmail.com>
2023-09-12 20:47:45 +0200

    RabbitMQ * Created new module for transactionHistory and started implementing subscriber and exchange.

---
+++ transactionHistory/src/main/java/com/pg3402/csgobank/transactionhistory/configuration/transactionEventHandler.java
@@ -0,0 +1,6 @@
+package com.pg3402.csgobank.transactionhistory.configuration;
+
+
+public class transactionEventHandler {
+void ost(final )
+}
---
+++ transactionHistory/src/test/java/com/pg3402/csgobank/transactionhistory/TransactionHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg3402.csgobank.transactionhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionHistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
