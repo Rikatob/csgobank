e3854a095a200acbcd1381b5822f74697e0401e0
Rikatob <rikatob@hotmail.com>
2023-09-17 21:51:34 +0200

    Transaction validator (NOT WORKING) * started testing ways to send direct messages from Vault to TransactionValidator.

--- vault/src/main/java/com/pg3402/csgobank/vault/VaultService.java
+++ vault/src/main/java/com/pg3402/csgobank/vault/VaultService.java
@@ -1,4 +1,31 @@
 package com.pg3402.csgobank.vault;
 
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class VaultService {
+
+
+    WebClient client = WebClient.create();
+
+
+    WebClient.ResponseSpec responseSpec  = client.get()
+            .uri("localhost:8080/transaction")
+            .retrieve();
+
+
+/*
+
+    public boolean validateTransaction() throws IOException {
+        URL url = new URL("localhost:8080/transaction");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        return Boolean.parseBoolean(con.getResponseMessage());
+    }
+*/
 }
