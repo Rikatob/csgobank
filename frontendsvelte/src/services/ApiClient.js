5e465b59b4e583097ee93bfd139f7b6123f33725
Rikatob <rikatob@hotmail.com>
2023-10-25 20:59:09 +0200

    Converting to Svelte * now render items with all info and a picture (chosen locally, not good) in a ok way.

--- frontendsvelte/src/services/ApiClient.js
+++ frontendsvelte/src/services/ApiClient.js
@@ -1,12 +1,23 @@
 class ApiClient {
 
-  static SERVER_URL = 'http://localhost:8000';
-  static GET_VAULT = '/vault';
-  static GET_ITEMS = '/vault/items';
+    static SERVER_URL = 'http://localhost:8000';
+    static GET_VAULT = '/vault';
+    static GET_ITEMS = '/vault/items';
 
-  static vault() {
-    return fetch(ApiClient.SERVER_URL + ApiClient.GET_VAULT)
-  }
+
+    static getItems() {
+        return fetch(ApiClient.SERVER_URL + ApiClient.GET_ITEMS)
+    }
+}
+
+export async function getItems() {
+    const res = await fetch(ApiClient.SERVER_URL + ApiClient.GET_ITEMS);
+
+    if (res.ok) {
+        return await res.json();
+    } else {
+        throw new Error("Request failed");
+    }
 }
 
 export default ApiClient;
