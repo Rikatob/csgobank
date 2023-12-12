e68b9a71fe3654ddc6e3b1e0bc3f6eb22bd98a9f
Fargekritt <Lullinj98@gmail.com>
2023-12-13 00:10:48 +0100

    Frontend changes integrated

--- transactionHistory/src/main/java/com/pg3402/csgobank/transactionhistory/transaction/TransactionRepository.java
+++ transactionHistory/src/main/java/com/pg3402/csgobank/transactionhistory/transaction/TransactionRepository.java
@@ -3,7 +3,11 @@ package com.pg3402.csgobank.transactionhistory.transaction;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction,Long> {
+
+    List<Transaction> findAllByItemId(long id);
 }
